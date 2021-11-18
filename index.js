f5ff798c8868c1e005c3aaca1b08f9450f9d28a7
SinuheJardinez <sinuhe.jh@gmail.com>
2021-11-18 13:48:07 -0600

    cervezas aleatorias y demas

--- index.js
+++ index.js
@@ -2,20 +2,58 @@
 const express = require("express")
 const app = express()
 const hbs = require("hbs")
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");//creamos nueva class con el punkAPI refiriendonos al appi de base sde datos de punkapi
+const punkAPI = new PunkAPIWrapper();//generamos una instancia para poder acceder a esta class
 
 require("dotenv").config()
 
 //2. MIDDLEWARES
-app.use(express.static('public'))
+app.use(express.static('public')) //activamos carpeta public, permite entrar a archivos e imagenes
 
 app.set("views", __dirname + "/views")
 app.set("view engine", "hbs")
+
+hbs.registerPartials(__dirname + "/views/partials")
 
 
 // 3. RUTAS
 app.get("/", (req, res) => {
 	res.render("home")
 })
+
+app.get("/random-beer", (req, res) => {
+    const randomBeers = punkAPI.getRandom()
+    randomBeers
+    .then((beers)=>{
+        res.render("random-beer",{
+            cheve:beers 
+        })
+    })
+})
+
+app.get("/beers", (req, res) => {
+   //generacion de promesas
+    const listBeers = punkAPI.getBeers()
+    
+    //evaluacion de promesa, si se cumple es .then, si no es .catch
+    listBeers
+    .then((beers) => { // SI LA PROMESA SE CUMPLIÓ EXITOSAMENTE
+        //console.log(beers)
+        res.render("beers",{
+            cheve:beers
+        })
+    })
+    .catch((error) => { // SI LA PROMESA NO SE CUMPLIÓ. ROMPISTE MI CORAZÓN.
+        console.log(error)
+    })
+  
+        //console.log(punkAPI) //aqui confirmamos nomas que la instancia funciona
+
+
+})
+
+
+
 
 
 
